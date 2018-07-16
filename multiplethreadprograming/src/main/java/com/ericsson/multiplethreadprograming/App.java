d772458472d1304cf343567fd8d4728423d80bf8
Joe Wang <joe.wang@ericsson.com>
2018-07-16 03:58:19 +0200

    Test to push
    Change-Id: I189c3cc5cba7c1ccf5864b4164153acc7f1774f2
    Signed-off-by: Joe Wang <joe.wang@ericsson.com>

---
+++ multiplethreadprograming/src/main/java/com/ericsson/multiplethreadprograming/App.java
@@ -0,0 +1,13 @@
+package com.ericsson.multiplethreadprograming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
